a7c3178fd571b94ff4f5f9d0d18f5e78644f1eb4
zhangchunsheng <zhangchunsheng423@gmail.com>
2023-06-08 00:04:48 +0800

    update

--- docker/config/constants.py
+++ docker/config/constants.py
@@ -38,7 +38,7 @@ ICE_SERVER_OVERRIDE = None
 # ]
 
 ICE_SERVER_BASE_URL = 'https://xvido365.com'
-ICE_SERVER_URL_TEMPLATE = '%s/randomcalling?key=%s'
+ICE_SERVER_URL_TEMPLATE = '%s/iceconfig?key=%s'
 ICE_SERVER_API_KEY = os.environ.get('ICE_SERVER_API_KEY')
 
 # Dictionary keys in the collider instance info constant.
